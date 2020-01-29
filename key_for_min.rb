273ce1cb8714a0aa189b294e6744c1cc9977cc98
Micah Banschick <micha.ban@gmail.com>
2020-01-29 15:27:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if name_hash.all?{|this, num| num >= name_hash[key]}
-      return name_hash[key]
+      return key
     end 
   end 
 end
