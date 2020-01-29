b572d04e9434f9fb0183c686fbbda533c7cfa4b9
Micah Banschick <micha.ban@gmail.com>
2020-01-29 14:52:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.find do |key|
+    key
+  end 
+  i = 0
+  j = 0
+  while i < name_hash.length do
+    while j < name_hash.length do 
+      if name_hash.all?{|num| num }
+  end 
 end
